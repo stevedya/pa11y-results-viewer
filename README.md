57a6d91f06e7040f4f9b39d25b70f32aaab967c6
Steven Steinwand <steven_ts@hotmail.com>
2023-07-05 07:52:02 -0600

    First Commit

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 ### Pa11y Audit Viewer
 Used to view pa11y audits in a clean way.
 
-### Compiling styles
+### Compiling styles for react app
 `npx tailwindcss -i ./css/tailwind.css -o ./css/main.css --watch`
 
 ### Compiling js 
+`npm install`
 `npm run watch`
 
 ### Checking pa11y
 This command will output a results.json which is used inside of app.js.
 If using a threshold enter the number when running the command.
 `pa11y-ci --threshold 10` 
+
+
+### how to view
+Open index.html in browser
--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
 import ReactDOM from "react-dom/client";
+
+// Add results file here
 import results from '../results.json'
 
 'use strict';
 
 const App = () => {
     const [resultsArray, setResultsArray] = useState(Object.entries(results.results));
+    const [selectedCodes, setSelectedCodes] = useState();
 
-    const filters = useMemo(() => {
+    const filterOptions = useMemo(() => {
         const filteredItems = [];
 
         // Not the most efficient but whatever
@@ -18,6 +21,52 @@ const App = () => {
         // Remove duplicates from that large list
         return [... new Set(filteredItems)]
     }, [resultsArray])
+
+
+    useEffect(() => {
+        setSelectedCodes(filterOptions.reduce((prev, current, index) => (
+            { ...prev, [current]: false }
+        ), {}))
+    }, [filterOptions])
+
+
+    // const filteredResults = useMemo(() => {
+    // if (selectedCodes && selectedCodes.length > 0) {
+    // return resultsArray.filter((item) => {
+
+    //     // Filter out the stuff in the results array
+    //     const resultsArray = item[1].filter((resultInfo) => {
+    //         return selectedCodes.includes(resultInfo['code'])
+    //      })
+
+    //      if (resultsArray.length > 0) {
+    //          return [item[0], resultsArray]
+    //      } 
+    //     //  TODO: Fix this filtering
+    // })
+    // }
+
+    // }, [resultsArray, selectedCodes])
+
+    const handleChange = (e) => {
+        setSelectedCodes((previousValues) => ({ ...previousValues, [e.target.value]: !e.target.checked }))
+    }
+
+    const renderResults = (result, index) => {
+        if (!result) {
+            return;
+        }
+        // if (filteredResults && filteredResults.length > 0) {
+        //     return filteredResults[1].map((item, itemIndex) => <LineItem item={item} key={`${index}-${itemIndex}`} />)
+        // }
+        else {
+            return result[1].map((item, itemIndex) => <LineItem item={item} key={`${index}-${itemIndex}`} />)
+        }
+    }
+
+    useEffect(() => {
+        renderResults();
+    }, [selectedCodes])
 
     return (
         <section className="py-8 md:py-12">
@@ -31,45 +80,52 @@ const App = () => {
                                 <p className="mt-2 text-lg text-gray-900 font-medium">Pages: {results.total}</p>
                                 <p className="mt-2 text-lg text-red-600 pl-6 font-medium">Errors: {results.errors}</p>
                             </div>
-                            <hr className="my-6"/>
-
-
+                            <hr className="my-6" />
                             <h3 className="font-medium mb-1 text-md mt-6">Filter by code</h3>
                             <div className="flex flex-wrap gap-6 p-3 rounded-md bg-gray-50">
                                 {
-                                    filters.map((filter, index) => (
-                                           <div className="inline-flex items-center" key={index}>
-                                               <input className="accent-pink-500 mr-2" value={filter} type="checkbox"/>
-                                               <span className="text-gray-600">{filter}</span>
-                                           </div>
+                                    filterOptions.map((filter, index) => (
+                                        <div className="inline-flex items-center" key={index}>
+                                            <input
+                                                // checked={selectedCodes[filter]}
+                                                onChange={(e) => handleChange(e)}
+                                                className="accent-pink-500 mr-2"
+                                                value={filter}
+                                                type="checkbox"
+                                            />
+                                            <span className="text-gray-600">{filter}</span>
+                                        </div>
                                     ))
                                 }
                             </div>
                         </div>
                     </div>
-                    {resultsArray && resultsArray.map((result, index) => (
+                    {resultsArray && resultsArray.length > 0 && resultsArray.map((result, index) => (
                         <div className="mt-8">
-                            <h2 className="text-xl md:text-2xl text-indigo-600 mb-2 font-bold">{result[0]}</h2>
-                            <div className="-mx-4 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg" >
-                                <table className="min-w-full divide-y divide-gray-300">
-                                    <thead className="bg-gray-50">
-                                        <tr>
-                                            <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Impact</th>
-                                            <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Code</th>
-                                            <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">Message</th>
-                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Context</th>
-                                            <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Runner</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="divide-y divide-gray-200 bg-white">
-                                        {
-                                            result[1].map((item, itemIndex) => <LineItem item={item} key={`${index}-${itemIndex}`} />)
-                                        }
-                                    </tbody>
-                                </table>
-                            </div>
+                            <h2 className="text-xl md:text-2xl text-indigo-600 mb-2 font-bold"><a target="_blank" href={result[0]}>{result[0]}</a></h2>
+                            {result[1] && result[1].length < 1 ?
+                                <p>Nothing to see here 🎉</p>
+                                :
+                                <div className="-mx-4 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg" >
+                                    <table className="min-w-full divide-y divide-gray-300">
+                                        <thead className="bg-gray-50">
+                                            <tr>
+                                                <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Impact</th>
+                                                <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Code</th>
+                                                <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">Message</th>
+                                                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Context</th>
+                                                <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Runner</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="divide-y divide-gray-200 bg-white">
+                                            {renderResults(result, index)}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            }
                         </div>
                     ))}
+
                 </div>
             </div>
         </section>
@@ -78,7 +134,7 @@ const App = () => {
 
 const LineItem = ({ item }) => {
     const { code, message, context, runner, runnerExtras } = item;
-    const { impact, help } = runnerExtras;
+    const { impact, help } = { ...runnerExtras };
 
     return (
         <tr>
